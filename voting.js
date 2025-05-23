a28d45c4432b2c13faece5e73b4ab3daad3e50bc
yourasshol <charles.edwards2@elev.vittra.se>
2025-05-23 10:04:52 +0200

    Update voting.js

--- voting.js
+++ voting.js
@@ -1,1 +1,9 @@
+function startMeeting() {
+    console.log("Emergency Meeting Called!");
+    // AI logic for voting
+}
 
+function vote(player) {
+    console.log(`${player} has been voted out.`);
+}
+#idk
